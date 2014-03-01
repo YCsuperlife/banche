f896be91511abf05fbff79f004513d71fb11225c
max <max@max-T410.(none)>
2014-03-01 11:36:58 +0800

    v1.0 ok

---
+++ serve.py
@@ -0,0 +1,64 @@
+#coding=utf-8
+
+from bottle import route, run, template, post, request
+import sqlite3 as db
+import json
+
+# DB API
+def init_db():
+    con = db.connect('user.db')
+    cur = con.cursor()
+    try:
+        cur.execute('create table user (name varchar(100), lon varchar(100), lat varchar(100), addr varchar(100))')
+    except:
+        print 'table existed'
+        pass
+
+def get_user_list():
+    con = db.connect('user.db')
+    cur = con.cursor()
+
+    cur.execute("select name,lon,lat,addr from user")
+    return cur.fetchall()
+
+def update_user(name, lon, lat, addr):
+    con = db.connect('user.db')
+    cur = con.cursor()
+
+    # 看看用户存在不
+    cur.execute("select * from user where name='%s'" % (name))
+    if len(cur.fetchall()) > 0:
+        cur.execute("update user set lon='%s',lat='%s',addr='%s' where name='%s'" % (lon, lat, addr, name))
+    else:
+        cur.execute("insert into user values('%s','%s','%s','%s')"%(name,lon,lat,addr))
+    con.commit()
+
+
+@route('/')
+def index():
+    init_db()
+    return template('index.html')
+
+@route('/result')
+def result():
+    init_db()
+    return template('result.html')
+
+@post('/api/adduser')
+def api_adduser():
+    name = request.forms.get('name')
+    lon = request.forms.get('lon')
+    lat = request.forms.get('lat')
+    addr = request.forms.get('address')
+
+
+    update_user(name,lon,lat,addr)
+    return "上传成功"
+
+@route('/api/list')
+def api_list():
+    ret = json.dumps(get_user_list())
+    return ret
+
+if __name__=="__main__":
+    run(host="0.0.0.0", port=8080)
